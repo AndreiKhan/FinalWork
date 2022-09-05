fac607cbe9679e34766998e99c292223b6acd144
«Andrei» <Spectacul9r@mail.ru>
2022-09-05 19:57:10 +0500

    Complete code

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] ReadToFillArray(string text)
+{
+    Console.Write(text);
+
+    string[] array = Console.ReadLine().Split(" ");
+
+    return array;
+}
+
+string[] CutLongLines(string[] inputArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3 && inputArray[i].Length >= 1)
+        {
+            count++;
+        }
+    }
+
+    if (count == 0)
+    {
+        string[] emptyArray = new string[1];
+        return emptyArray;
+    }
+
+    string[] outputArray = new string[count];
+    count = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3 && inputArray[i].Length >= 1)
+        {
+            outputArray[count] = inputArray[i];
+            count++;
+        }
+    }
+    return outputArray;
+}
+
+void PrintArray(string[] inputArray)
+{
+    int i = 0;
+    Console.Write("[");
+
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write("\"" + inputArray[i] + "\", ");
+        i++;
+    }
+
+    Console.WriteLine("\"" + inputArray[i] + "\"]");
+}
+
+string[] arrayLines = ReadToFillArray("Введите что хотите, через пробел: ");
+
+string[] newArray = CutLongLines(arrayLines);
+
+PrintArray(newArray);
